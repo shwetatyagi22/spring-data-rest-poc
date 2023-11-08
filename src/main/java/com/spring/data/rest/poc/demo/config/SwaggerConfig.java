f155d84d69b10ccb959ec7186056c4d444b625b6
Shweta Tyagi <shweta.tyagi@seera.sa>
2023-11-08 13:57:40 +0400

    first commit

---
+++ src/main/java/com/spring/data/rest/poc/demo/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package com.spring.data.rest.poc.demo.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+/*
+
+@Configuration
+@EnableSwagger3
+public class SwaggerConfig {
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any()).build();
+  }
+}*/
